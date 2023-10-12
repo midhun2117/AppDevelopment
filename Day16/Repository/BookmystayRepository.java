2f905473efaa1ffbcddbd27f4a41629e2fbdbfc9
midhun2117 <120155668+midhun2117@users.noreply.github.com>
2023-10-12 12:38:36 +0530

    Add files via upload

---
+++ Day16/Repository/BookmystayRepository.java
@@ -0,0 +1,12 @@
+package com.midhun.bookmystay.Repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.midhun.bookmystay.Entity.Bookmystay;
+
+public interface BookmystayRepository extends JpaRepository<Bookmystay,Long> {
+
+	
+
+}
